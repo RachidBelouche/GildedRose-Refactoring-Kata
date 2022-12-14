c467c873e654a5381210719d8b58a44c496aa1a7
Rachid Belouche <rachid.belouche93@gmail.com>
2022-12-14 16:27:08 +0100

    Refactor with methods

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -10,82 +10,107 @@ namespace GildedRoseKata
             this.Items = Items;
         }
 
+        private void UpdateAgedBrie(Item item)
+        {
+            if (item.SellIn >= 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn < 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+        }
+
+        private void UpdateBackstage(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn >= 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+        }
+
+        private void UpdateSulfuras(Item item)
+        {
+            if (item.SellIn >= 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            if (item.SellIn < 0)
+                item.Quality = item.Quality - item.Quality;
+        }
+
+        private void UpdateTheRest(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+            item.SellIn = item.SellIn - 1;
+            if (item.SellIn >= 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            if (item.SellIn < 0)
+                item.Quality = item.Quality - item.Quality;
+            if (item.SellIn < 0 && item.Quality > 0)
+            {
+
+                item.Quality = item.Quality - 1;
+
+            }
+        }
+
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
             {
                 if (Items[i].Name == "Aged Brie")
                 {
-                    if (Items[i].SellIn >= 0 && Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    UpdateAgedBrie(Items[i]);
                 }
                 else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                    if (Items[i].SellIn >= 0 && Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    UpdateBackstage(Items[i]);
                 }
                 else if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
                 {
-                    if (Items[i].SellIn >= 0 && Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-
-                    if (Items[i].SellIn < 0)
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    UpdateSulfuras(Items[i]);
                 }
-                else if (Items[i].Name != "Conjured Mana Cake")
+                else if (Items[i].Name == "Conjured Mana Cake")
                 {
 
                 }
                 else
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                    if (Items[i].SellIn >= 0 && Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
-
-                    if (Items[i].SellIn < 0)
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    if (Items[i].SellIn < 0 && Items[i].Quality > 0)
-                    {
-
-                        Items[i].Quality = Items[i].Quality - 1;
-
-                    }
+                    UpdateTheRest(Items[i]);
                 }
             }
+            return;
         }
     }
 }
